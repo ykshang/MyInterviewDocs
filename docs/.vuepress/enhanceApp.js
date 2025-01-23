8159fc9df7d6af451bc01435c5c9716e235a6e06
kangkang shang <535634238@qq.com>
2025-01-23 14:07:23 +0800

    更新文档

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,8 +6,6 @@ import Codes from './componets/Codes.vue'
 import CodeBold from './componets/CodeBold.vue'
 import 'font-awesome/css/font-awesome.css';// 引入font-awesome图表字体组件
 
-
-Codes
 export default ({ Vue }) => {
   Vue.use(Element);
   Vue.component('LinkIcon', LinkIcon)
