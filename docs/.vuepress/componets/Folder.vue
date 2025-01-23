4460ee6d6c40bcec196671def1a9ce3bd772fb98
kangkang shang <535634238@qq.com>
2025-01-23 14:50:33 +0800

    更新文档

--- docs/.vuepress/componets/Folder.vue
+++ docs/.vuepress/componets/Folder.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="self-folder">
-    <slot></slot>
-  </div>
+  <span class="self-folder">
+    <span class="fa fa-folder self-icon"></span>
+    <span class="self-path">{{ path }}</span>
+  </span>
 </template>
 
 <script>
 export default {
   name: 'Folder',
+  props: ["path"],
+
 };
 </script>
 <style scoped>
 .self-folder {
   display: inline-block;
-  margin: auto 2px;
-  padding: 1px 4px;
-  font-size: 85%;
+  margin: 0 2px;
+  padding: 0px 4px;
+  line-height: 1.5;
   font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  background-color: #fcf4cf72;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.self-folder:hover>.self-path {
+  color: #a98b05;
+}
+.self-icon {
+  color: #ffb829;
+  font-size: 1.1rem;
+  margin-right: 3px;
+}
+.self-path {
+  color: #5c4b02;
+  text-decoration: underline;
+  font-size: 96%;
 }
 </style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,11 +4,14 @@ import './styles/main.css';// 引入自定义样式
 import LinkIcon from './componets/LinkIcon.vue'
 import Codes from './componets/Codes.vue'
 import CodeBold from './componets/CodeBold.vue'
+import Folder from './componets/Folder.vue'
 import 'font-awesome/css/font-awesome.css';// 引入font-awesome图表字体组件
 
 export default ({ Vue }) => {
   Vue.use(Element);
-  Vue.component('LinkIcon', LinkIcon)
-  Vue.component('Codes', Codes)
-  Vue.component('CodeBold', CodeBold)
+  Vue.component('LinkIcon', LinkIcon);
+  Vue.component('Codes', Codes);
+  Vue.component('CodeBold', CodeBold);
+  Vue.component('Folder', Folder);
 };
+ 
