34909c056cb2c4591219577c175f3c9a0ad6e5d2
kangkang shang <535634238@qq.com>
2025-01-24 18:35:30 +0800

    更新文档

---
+++ docs/.vuepress/componets/Param.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="basic" title="参数格式">
+    {{ text }}
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Param',
+  props: ["text"],
+  data () {
+    return {
+    };
+  },
+  mounted () {
+  }
+};
+</script>
+<style scoped>
+.basic {
+  display: inline-block;
+  margin: auto 2px;
+  padding: 2px 4px;
+  font-size: 90%;
+  line-height: 1.3rem;
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  background-color: #f3f5f7;
+  color: #42b983;
+  font-weight: bold;
+}
+.basic:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,6 +5,7 @@ import LinkIcon from './componets/LinkIcon.vue'
 import Codes from './componets/Codes.vue'
 import CodeBold from './componets/CodeBold.vue'
 import Folder from './componets/Folder.vue'
+import Param from './componets/Param.vue'
 import 'font-awesome/css/font-awesome.css';// 引入font-awesome图表字体组件
 
 export default ({ Vue }) => {
@@ -13,5 +14,6 @@ export default ({ Vue }) => {
   Vue.component('Codes', Codes);
   Vue.component('CodeBold', CodeBold);
   Vue.component('Folder', Folder);
+  Vue.component('Param', Param);
 };
  
