64969ec8f917b4bc00ffc2898e56bf025e305a83
kangkang shang <535634238@qq.com>
2025-03-03 14:55:35 +0800

    1

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -2,11 +2,16 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  // { text: '博客', link: '/blog/' },
+  { text: '按标签查找', link: '/blog/tags/' },
+  // { text: '归档', link: '/blog/archives/' },
   {
-    text: '笔记',
-    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+    text: '前端基础',
+    items: [{
+      text: '示例',
+      items: [{
+        text: '示例1', link: '/blog/'
+      }]
+    }]
   },
 ])
