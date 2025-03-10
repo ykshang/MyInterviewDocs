bb4fff22c3e68febf201fbd467d21a4fbeddc25a
kangkang shang <535634238@qq.com>
2025-03-10 23:48:54 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -110,28 +110,28 @@ export default defineUserConfig({
        *  markdown power
        * @see https://theme-plume.vuejs.press/config/plugin/markdown-power/
        */
-      // markdownPower: {
-      //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
-      //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
-      //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
-      //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
-      //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
-      //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
-      //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
-      //   icons: true,        // 启用内置图标语法  :[icon-name]:
-      //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
-      //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
-      //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
-      //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
-      //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
-      //   demo: true,         // 启用 demo 容器  ::: demo
-      //   repl: {             // 启用 代码演示容器
-      //     go: true,         // ::: go-repl
-      //     rust: true,       // ::: rust-repl
-      //     kotlin: true,     // ::: kotlin-repl
-      //   },
-      //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
-      // },
+      markdownPower: {
+        // pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
+        // caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
+        // plot: true,         // 启用隐秘文本语法 !!xxxx!!
+        // bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
+        // youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
+        // artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
+        // audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
+        icons: true,        // 启用内置图标语法  :[icon-name]:
+        // codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
+        // replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
+        // codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
+        // jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
+        // npmTo: true,        // 启用 npm-to 容器  ::: npm-to
+        demo: true,         // 启用 demo 容器  ::: demo
+        // repl: {             // 启用 代码演示容器
+        //   go: true,         // ::: go-repl
+        //   rust: true,       // ::: rust-repl
+        //   kotlin: true,     // ::: kotlin-repl
+        // },
+        imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+      },
 
       /**
        * 在 Markdown 文件中导入其他 markdown 文件内容。
