7c22f84986ae7effe1f7a4225417a2427cbb0003
kangkang shang <535634238@qq.com>
2025-03-11 16:39:17 +0800

    11

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -2,7 +2,7 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
-  { text: '杂谈', link: '/blog/' },
+  { text: '随想笔记', link: '/blog/' },
   // { text: '杂谈', link: '/blog/tags/' },
   // { text: '归档', link: '/blog/archives/' },
   {
