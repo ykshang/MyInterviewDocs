3ad25acd08a13b405c4cda0cb080cb2c67568d6f
kangkang shang <535634238@qq.com>
2025-03-11 23:10:55 +0800

    1

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+## 安装依赖
+npm install
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git config user.email "535634238@qq.com"
+git config --global user.name "kangkang shang"
+git add -A
+git commit -m 'Automatic build'
+
+# 发布到gh-pages分支
+git checkout -B master
+git push -f git@github.com:ykshang/MyInterviewDocs.git master:gh-pages
+
+cd -
